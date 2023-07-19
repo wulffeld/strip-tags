916a780c3d8da4075b3b3d154d1b4ad6413a069e
Martin Moen Wulffeld <martin@wulffeld.dk>
2023-07-19 14:01:42 +0200

    First commit

---
+++ spec/strip_tags/allow_empty_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+module MockAttributes
+  def self.included(base)
+    base.attribute :foo
+    base.attribute :bar
+  end
+end
+
+class StripAllowEmpty < Tableless
+  include MockAttributes
+  strip_tags allow_empty: true
+end
+
+describe "StripTags: allow empty" do
+  before(:each) do
+    @init_params = {
+      foo: "foo<script>alert('xss')</script>",
+      bar: "",
+    }
+  end
+
+  it "strips and allow empty" do
+    record = StripAllowEmpty.new(@init_params)
+    record.valid?
+    expect(record.foo).to eq("foo")
+    expect(record.bar).to eq("")
+  end
+
+  it "strips and allow empty always" do
+    record = StripAllowEmpty.new(@init_params)
+    record.valid?
+    record.assign_attributes(@init_params)
+    record.valid?
+    expect(record.foo).to eq("foo")
+    expect(record.bar).to eq("")
+  end
+end
---
+++ spec/strip_tags/other_validations_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+class CoexistWithOtherValidations < Tableless
+  attribute :number, type: Integer
+
+  strip_tags
+
+  validates :number, {
+    numericality: { only_integer: true,  greater_than_or_equal_to: 1000 },
+    allow_blank: true
+  }
+end
+
+describe "StripTags: other validations" do
+  before(:each) do
+    @init_params = {
+      foo:  "foo<script>alert('xss')</script>",
+      bar:  "<script>alert('xss')</script>bar",
+      baz:  "",
+      bang: " ",
+      foz:  " foz  foz",
+      fiz:  "fiz \n  fiz",
+      qax:  "\n\t ",
+      qux:  "\u200B"
+    }
+  end
+
+  it "coexists with other validations" do
+    record = CoexistWithOtherValidations.new
+    record.number = 1000.1
+    expect(record.valid?).to be false
+    expect(record.errors.include?(:number)).to be true
+
+    record = CoexistWithOtherValidations.new(number: " 1000.2 ")
+    expect(record.valid?).to be false
+    expect(record.errors.include?(:number)).to be true
+  end
+end
